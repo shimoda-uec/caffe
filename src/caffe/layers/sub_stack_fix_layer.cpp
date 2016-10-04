22793431854beda4431d42f97af8afd581304db0
shmoda-k <shimoda-k@mm.inf.uec.ac.jp>
2016-10-04 10:20:27 +0900

    '16/10/04'

--- src/caffe/layers/sub_stack_fix_layer.cpp
+++ src/caffe/layers/sub_stack_fix_layer.cpp
@@ -9,14 +9,12 @@ namespace caffe {
 template <typename Dtype>
 void SubStackFixLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  AddSweeperParameter sweeper_param = this->layer_param_.add_sweeper_param();
-  sweepern_=sweeper_param.sweepern();
 }
 
 template <typename Dtype>
 void SubStackFixLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
-  top[0]->Reshape(sweepern_,1,bottom[0]->height(),bottom[0]->width());
+  top[0]->Reshape(bottom[1]->num(),1,bottom[0]->height(),bottom[0]->width());
 }
 
 template <typename Dtype>
@@ -25,19 +23,16 @@ void SubStackFixLayer<Dtype>::Forward_cpu(
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
   const int inner_num=bottom[0]->height()*bottom[0]->width()*bottom[0]->channels();
-  for (int n = 0; n < 1; ++n) {
-  	for (int e1=0;e1<sweepern_;++e1){
+  for (int e1=0;e1<bottom[1]->num();++e1){
           caffe_set(inner_num, Dtype(0), top_data);
-  	   for (int e2=0;e2<sweepern_;++e2){
+  	   for (int e2=0;e2<bottom[1]->num();++e2){
 	       if(e1!=e2){
 		  	    caffe_axpy(inner_num, Dtype(1.0), bottom_data , top_data);
 	        }
            bottom_data += bottom[0]->offset(1, 0);
   	   }
          top_data += top[0]->offset(1, 0);
-  	}
   }
-
 }
 
 template <typename Dtype>
